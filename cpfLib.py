5b14e48875395c320f7261c50efcd1cca7cf6a26
Adriel Teles <adrielteles@outlook.com>
2022-12-04 21:38:22 -0300

    implemented version 1.0.0

---
+++ cpfLib.py
@@ -0,0 +1,70 @@
+import random
+
+def cpfValidator(cpf:str):
+    cpf_refactory = cpf.replace('.','').replace('-','').replace(' ','')
+    invalid_cpf = cpf_refactory[0]*len(cpf_refactory)
+
+    if invalid_cpf == cpf_refactory:
+        return False
+    
+    nove_digitos = cpf_refactory[:9]
+    contador_regressivo = 10
+    resultado = 0
+    for digito in nove_digitos:
+        resultado += int(digito)*contador_regressivo
+        contador_regressivo -=1
+
+    primeiro_digito = (resultado*10) % 11
+    primeiro_digito = primeiro_digito if primeiro_digito <=9 else 0
+    
+
+    dez_digitos = nove_digitos + str(primeiro_digito)
+    contador_regressivo_2 = 11
+    resultado2 = 0
+
+    for digito in dez_digitos:
+        resultado2 += int(digito)*contador_regressivo_2
+        contador_regressivo_2 -=1
+
+    segundo_digito = (resultado2*10) % 11
+    segundo_digito = segundo_digito if segundo_digito <=9 else 0
+
+    result_cpf = f'{nove_digitos}{primeiro_digito}{segundo_digito}'
+
+    return True if result_cpf == cpf_refactory else False
+
+
+def cpfGenerator(quantity:int):
+
+    if int(quantity) > 0:
+
+        for _ in range(int(quantity)):
+            nove_digitos = ''
+            for _ in range(9):
+                nove_digitos += str(random.randint(0,9))
+
+            contador_regressivo = 10
+            resultado = 0
+            for digito in nove_digitos:
+                resultado += int(digito)*contador_regressivo
+                contador_regressivo -=1
+
+            primeiro_digito = (resultado*10) % 11
+            primeiro_digito = primeiro_digito if primeiro_digito <=9 else 0
+            
+
+            dez_digitos = nove_digitos + str(primeiro_digito)
+            contador_regressivo_2 = 11
+            resultado2 = 0
+
+            for digito in dez_digitos:
+                resultado2 += int(digito)*contador_regressivo_2
+                contador_regressivo_2 -=1
+
+            segundo_digito = (resultado2*10) % 11
+            segundo_digito = segundo_digito if segundo_digito <=9 else 0
+
+            result_cpf = f'{nove_digitos}{primeiro_digito}{segundo_digito}'
+            print(result_cpf)
+    else:
+        print('Invalid Quantity.')
---
+++ main.py
@@ -0,0 +1,32 @@
+import cpfLib as cpfLib
+
+while True:
+
+    try:
+        print('-'*25)
+        print('Brazilian CPF tool')
+        print('-'*25)
+        print('options:')
+        print('1. Validate')
+        print('2. Generate')
+        print('3. Exit')
+        option = int(input('Option: '))
+        if option == 1:
+            print('-'*25)
+            print('CPF Validator')
+            print('-'*25)
+            cpf = input('Enter the CPF: ')
+            if cpfLib.cpfValidator(cpf):
+                print('valid CPF')
+            else:
+                print('invalid CPF')
+        elif option == 2:
+            print('-'*25)
+            print('CPF Generator')
+            print('-'*25)
+            num_cpf = input('How many CPF: ')
+            cpfLib.cpfGenerator(num_cpf)
+    except ValueError:
+        print('Invalid Option')
+    except EOFError:
+        break
